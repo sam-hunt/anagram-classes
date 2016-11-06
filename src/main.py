75ab1e01626702a936565925ba2058e313b23123
Sam Hunt <s2112201@ipc.ac.nz>
2016-11-06 14:57:02 +1300

    Use python lib hash function.

--- src/main.py
+++ src/main.py
@@ -15,23 +15,6 @@ def loadwords():
     f.close
     words = allwords.lower().split('\n')
     return words
-#===============================================================================
-# 
-# def hash3(word):                #4-byte folding string hash function
-#     ilen, sum = len(word)/4, 0
-#     for j in xrange(ilen):
-#         subword = word[j*4:(j*4)+4]
-#         mult, swl = 1, len(subword)
-#         for k in xrange(swl):
-#             sum += ord(subword[k]) * mult
-#             mult *= 256
-#     subword = word[ilen*4:]
-#     mult = 1
-#     for k in xrange(len(subword)):
-#         sum += ord(subword[k])*mult
-#         mult *= 256
-#     return sum
-#===============================================================================
 
 keys = defaultdict(list)
 words = loadwords()             #precondition: =split list of lower case words
